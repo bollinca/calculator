6667150cca394e09fb1d13748a13088a7c1f9d91
bollinca <51376777+bollinca@users.noreply.github.com>
2019-07-28 14:56:56 -0400

    Add operator function

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-function add(num1, num2) {
+const add = (num1, num2) => {
     return num1 + num2;
-}
+};
 
-function subtract(num1, num2) {
+const subtract = (num1, num2) => {
     return num1 - num2;
-}
+};
 
-function multiply(num1, num2 = 1) {
+const multiply = (num1, num2 = 1) => {
     return num1 * num2;
-}
+};
 
-function divide(num1, num2 = 1) {
+const divide = (num1, num2 = 1) => {
     return num1 / num2;
+};
+
+function operate(num1, num2, operator) {
+    return operator(num1, num2);
 }
