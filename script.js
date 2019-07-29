11c63ff829bdc6dd5448ea8d26bec96cc7609dbb
bollinca <51376777+bollinca@users.noreply.github.com>
2019-07-29 09:54:41 -0400

    Add clear button functionality, draft operator button events, edit display

--- script.js
+++ script.js
@@ -18,16 +18,42 @@ const operate = (num1, num2, operator) => {
     return operator(num1, num2);
 }
 
-const changeDisplay = (value) => {
-    return display.textContent = value;
+const updateDisplay = () => {
+    return display.textContent = newDisplayValue;
 }
 
+const clearDisplay = () => {
+    newDisplayValue = '';
+    updateDisplay();
+}
 
 const display = document.getElementById('display');
+display.textContent = 'Hello, please press a button.'
+let displayValue = display.textContent;
+let oldDisplayValue = '';
+let newDisplayValue = '';
 
-function assignButtonEvents() {
-    const buttons = Array.from(document.querySelectorAll('button'));
-    buttons.forEach(button => button.addEventListener('click', () => changeDisplay(button.textContent)));
+
+function assignNumButtonEvents() {
+    const buttons = Array.from(document.querySelectorAll('.num-button'));
+    buttons.forEach(button => button.addEventListener('click', () => (newDisplayValue = newDisplayValue + button.textContent)));
+
 }
 
-assignButtonEvents();
+function assignOperButtonEvents () {
+    const clearButton = document.querySelector('#clear')
+    clearButton.addEventListener('click', () => clearDisplay());
+    const addButton = document.querySelector('#add');
+    addButton.addEventListener('click', () => console.error('FIX ADDITION-EVENT'));
+    const subButton = document.querySelector('#sub');
+    subButton.addEventListener('click', () => console.error('FIX SUBTRACTION-EVENT'));
+    const multButton = document.querySelector('#mult');
+    multButton.addEventListener('click', () => console.error('FIX MULTIPLY-EVENT'));
+    const diviButton = document.querySelector('#divi');
+    diviButton.addEventListener('click', () => console.error('FIX DIVISION-EVENT'));
+    const equalButton = document.querySelector('#equal');
+    equalButton.addEventListener('click', () => console.error('FIX EQUAL-EVENT'));
+}
+
+assignNumButtonEvents();
+assignOperButtonEvents();
