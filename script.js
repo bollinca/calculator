324367602e383ec0c137f4725d9e1c0a976686b9
bollinca <51376777+bollinca@users.noreply.github.com>
2019-07-29 14:00:42 -0400

    Add operator selection, introduce fun new bugs

--- script.js
+++ script.js
@@ -1,59 +1,66 @@
 const add = (num1, num2) => {
-    return num1 + num2;
+    solution = num1 + num2;
 };
 
 const subtract = (num1, num2) => {
-    return num1 - num2;
+    solution =  num1 - num2;
 };
 
 const multiply = (num1, num2 = 1) => {
-    return num1 * num2;
+    solution =  num1 * num2;
 };
 
 const divide = (num1, num2 = 1) => {
-    return num1 / num2;
+    solution =  num1 / num2;
 };
 
 const operate = (num1, num2, operator) => {
-    return operator(num1, num2);
-}
+    solution = operator(num1, num2);
+    display.textContent = solution;
+    prevCalcInput = currentCalcInput;
+    currentCalcInput = '';
+    updateDisplay();
+};
 
 const updateDisplay = () => {
-    return display.textContent = newDisplayValue;
+    return display.textContent;
 }
 
-const clearDisplay = () => {
-    newDisplayValue = '';
+const clearAll = () => {
+    prevCalcInput = '';
+    currentCalcInput = '';
     updateDisplay();
+}
+
+function assignNumButtonEvents() {
+    const numButtons = Array.from(document.querySelectorAll('.num-button'));
+    numButtons.forEach(numButton => numButton.addEventListener('click', () => (currentCalcInput = currentCalcInput + numButton.textContent)));
+}
+
+function assignOperButtonEvents () {
+    const clearButton = document.querySelector('#clear')
+    clearButton.addEventListener('click', () => clearAll());
+    const addButton = document.querySelector('#add');
+    addButton.addEventListener('click', selectedOpp = add);
+    const subButton = document.querySelector('#sub');
+    subButton.addEventListener('click', selectedOpp = subtract);
+    const multButton = document.querySelector('#mult');
+    multButton.addEventListener('click', selectedOpp = multiply);
+    const diviButton = document.querySelector('#divi');
+    diviButton.addEventListener('click', selectedOpp = divide);
+    const equalButton = document.querySelector('#equal');
+    equalButton.addEventListener('click', function () {
+        operate(prevCalcInput, currentCalcInput, selectedOpp)
+    });
 }
 
 const display = document.getElementById('display');
 display.textContent = 'Hello, please press a button.'
 let displayValue = display.textContent;
-let oldDisplayValue = '';
-let newDisplayValue = '';
-
-
-function assignNumButtonEvents() {
-    const buttons = Array.from(document.querySelectorAll('.num-button'));
-    buttons.forEach(button => button.addEventListener('click', () => (newDisplayValue = newDisplayValue + button.textContent)));
-
-}
-
-function assignOperButtonEvents () {
-    const clearButton = document.querySelector('#clear')
-    clearButton.addEventListener('click', () => clearDisplay());
-    const addButton = document.querySelector('#add');
-    addButton.addEventListener('click', () => console.error('FIX ADDITION-EVENT'));
-    const subButton = document.querySelector('#sub');
-    subButton.addEventListener('click', () => console.error('FIX SUBTRACTION-EVENT'));
-    const multButton = document.querySelector('#mult');
-    multButton.addEventListener('click', () => console.error('FIX MULTIPLY-EVENT'));
-    const diviButton = document.querySelector('#divi');
-    diviButton.addEventListener('click', () => console.error('FIX DIVISION-EVENT'));
-    const equalButton = document.querySelector('#equal');
-    equalButton.addEventListener('click', () => console.error('FIX EQUAL-EVENT'));
-}
+let prevCalcInput = '';
+let currentCalcInput = '';
+let selectedOpp;
+let solution;
 
 assignNumButtonEvents();
 assignOperButtonEvents();
