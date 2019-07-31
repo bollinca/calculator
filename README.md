16707671734a0693c829a1df00a8d099129eed08
bollinca <51376777+bollinca@users.noreply.github.com>
2019-07-31 10:27:23 -0400

    Move and refactor functions for human readability

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 A simple, javascript based calculator.
+ - Supports keyboards, please use "delete" to clear.
+ - Operators are used as modes. After clicking an operator once, the same operator will remain active until another is selected.
+ - Backspace will erase one digit from your current displayed value.
--- script.js
+++ script.js
@@ -1,80 +1,76 @@
-const add = (num1, num2) => {
-    return solution = +num1 + +num2;
-};
+function setOperListen() {
+    const oppButtons = Array.from(document.querySelectorAll('.opp-button'));
+    oppButtons.forEach(oppButton => oppButton.addEventListener('click', () => {
+        if (prevNumInput !== '' && currentNumInput !== '') {
+            updateDisplay(operate(prevNumInput, currentNumInput, currentOper));
+        } else {
+            return;
+        }
+    }));
+    oppButtons.forEach(oppButton => oppButton.addEventListener('click', storeInput));
+    
+    const addButton = document.querySelector('#add');
+    addButton.addEventListener('click', () => currentOper = add);
+    const subButton = document.querySelector('#sub');
+    subButton.addEventListener('click', () => currentOper = subtract);
+    const multButton = document.querySelector('#mult');
+    multButton.addEventListener('click', () => currentOper = multiply);
+    const diviButton = document.querySelector('#divi');
+    diviButton.addEventListener('click', () => currentOper = divide);
+}
 
-const subtract = (num1, num2) => {
-    return solution = num1 - num2;
-};
+function updateDisplay(input) {
+    displayField.textContent = input;
+}
 
-const multiply = (num1, num2 = 1) => {
-    return solution = num1 * num2;
-};
+function operate(num1, num2, operation) {
+    if(operation === divide && num2 === '0') {
+        return ('Can\'t divide by zero.');
+    } else {
+        result = operation(num1, num2);
+        return result = roundDecimals(result);
+    }
+}
 
-const divide = (num1, num2 = 1) => {
+function roundDecimals(result) {
+    if (result.toString().length > 6) {
+        return result = (Math.round(result * 10000) / 10000);
+    }
+    else {
+        return result
+    }
+}
+
+function add(num1, num2) {
+    return result = +num1 + +num2;
+}
+
+function subtract(num1, num2) {
+    return result = num1 - num2;
+}
+
+function multiply(num1, num2 = 1) {
+    return result = num1 * num2;
+}
+
+function divide(num1, num2 = 1) {
     if (num2 == 0) {
         clearAll();
         return; 
     } else {
-        return solution = num1 / num2;
-    }
-};
-
-const roundDecimals = (longDecimal) => {
-    if (longDecimal.toString().length > 6) {
-        return longDecimal = (Math.round(longDecimal * 10000) / 10000);
-    }
-    else {
-        return longDecimal
-    }
+        return result = num1 / num2;
+    }    
 }
 
-const operate = (num1, num2, operator) => {
-    if(operator === divide && num2 === '0') {
-        return ('Can\'t divide by zero.');
-    } else {
-        solution = operator(num1, num2);
-        return solution = roundDecimals(solution);
-    }
-};
-
-function removeOneCharacter(value) {
-    backspaceArray = Array.from(value);
-        backspaceArray.pop();
-        value = backspaceArray.join('');
-        updateDisplay(value);
-        return value;
-}
-
-const useBackspace = () => {
-    if (currentNumInput !== '') {
-        return currentNumInput = removeOneCharacter(currentNumInput);
-    } else if (solution) {
-        solution = solution.toString();
-        return solution = removeOneCharacter(solution);
-    }
-}
-
-const storeInput = () => {
-    if (solution || solution === 0) {
-        prevNumInput = solution;
+function storeInput() {
+    if (result || result === 0) {
+        prevNumInput = result;
         currentNumInput = '';
     } else {
         prevNumInput = currentNumInput;
         currentNumInput = '';
     }
 }
-
-const clearAll = () => {
-    prevNumInput = '';
-    currentNumInput = '';
-    solution = '';
-    currentOper = '';
-    updateDisplay('--------');
-}
-
-function updateDisplay(input) {
-    displayField.textContent = input;
-};
 
 function setNumListen() {
     const numButtons = Array.from(document.querySelectorAll('.num-button'));
@@ -89,35 +85,6 @@ function setNumListen() {
             updateDisplay(currentNumInput);
         }
     });
-}
-
-function setOperListen() {
-    const oppButtons = Array.from(document.querySelectorAll('.opp-button'));
-    oppButtons.forEach(oppButton => oppButton.addEventListener('click', () => {
-        if (prevNumInput !== '' && currentNumInput !== '') {
-            updateDisplay(operate(prevNumInput, currentNumInput, currentOper));
-        } else {
-            return;
-        }
-    }));
-    oppButtons.forEach(oppButton => oppButton.addEventListener('click', storeInput));
-
-    const addButton = document.querySelector('#add');
-    addButton.addEventListener('click', () => currentOper = add);
-    const subButton = document.querySelector('#sub');
-    subButton.addEventListener('click', () => currentOper = subtract);
-    const multButton = document.querySelector('#mult');
-    multButton.addEventListener('click', () => currentOper = multiply);
-    const diviButton = document.querySelector('#divi');
-    diviButton.addEventListener('click', () => currentOper = divide);
-}
-
-function activateButton(e) {
-    const keyboardInput = document.querySelector(`button[data-key='${e.key}']`);
-    if (keyboardInput) {
-        e.preventDefault();
-        keyboardInput.click();
-    }
 }
 
 function setMiscListen() {
@@ -136,14 +103,46 @@ function setMiscListen() {
     backspaceButton.addEventListener('click', () => useBackspace());
 }
 
+function clearAll() {
+    prevNumInput = '';
+    currentNumInput = '';
+    result = '';
+    currentOper = '';
+    updateDisplay('--------');
+}
+
+function useBackspace() {
+    if (currentNumInput !== '') {
+        return currentNumInput = popFrom(currentNumInput);
+    } else if (result) {
+        result = result.toString();
+        return result = popFrom(result);
+    }
+}
+
+function popFrom(value) {
+    backspaceArray = Array.from(value);
+        backspaceArray.pop();
+        value = backspaceArray.join('');
+        updateDisplay(value);
+        return value;
+}
+
+function activateButton(e) {
+    const keyboardInput = document.querySelector(`button[data-key='${e.key}']`);
+    if (keyboardInput) {
+        e.preventDefault();
+        keyboardInput.click();
+    }
+}
+
 let prevNumInput = '';
 let currentNumInput = '';
-let solution = '';
+let result = '';
 let currentOper = '';
 
 const displayField = document.getElementById('display');
 updateDisplay('--------');
-
 
 window.addEventListener('keydown', activateButton);
 setNumListen();
